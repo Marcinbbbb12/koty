f7b0d651414d951262d9d7ac9df9311a5008dcea
Marcin <uT_3777@zsp3zamosc>
2024-11-06 08:35:23 +0100

    Dodaj pliki projektów.

---
+++ koty/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace koty
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            string link = "https://cataas.com/cat/says/" + input.Text;
+           
+            kot.Source = link;
+        }
+
+        private void OnCounterClicked2(object sender, EventArgs e)
+        {
+            string link = "https://cataas.com/cat/gif";
+
+            kot.Source = link;
+        }
+        private void OnCounterClicked3(object sender, EventArgs e)
+        {
+            string link = "https://cataas.com/cat/says/hello?fontSize=50&fontColor=red";
+
+            kot.Source = link;
+        }
+        private void OnCounterClicked4(object sender, EventArgs e)
+        {
+            string link = "https://cataas.com/cat/gif/says/" + input.Text;
+
+            kot.Source = link;
+        }
+    }
+
+}
